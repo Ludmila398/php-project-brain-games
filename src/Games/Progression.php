e15f211a823d6528feb97b6c59c90ce8949012fa
Liudmila <liudmilagubanova18@gmail.com>
2023-03-17 13:02:03 +0400

    add changes

--- src/Games/Progression.php
+++ src/Games/Progression.php
@@ -10,13 +10,12 @@ use const Engine\NUMBER_OF_ROUNDS;
 
 const QUESTION = 'What number is missing in the progression?';
 
-function calculate(int $progrStep, int $progrStart): array
+function generateProgression(int $progrStep, int $progrStart): array
 {
     $progression = [];
-    $temp = $progrStart;
     for ($i = 0; $i < 10; $i++) {
-        $progression[] = $temp;
-        $temp += $progrStep;
+        $progression[] = $progrStart;
+        $progrStart += $progrStep;
     }
     return $progression;
 }
@@ -27,7 +26,7 @@ function startGetProgression()
     for ($i = 0; $i < NUMBER_OF_ROUNDS; $i++) {
         $progrStep = rand(2, 7);
         $progrStart = rand(2, 15);
-        $progrArray = calculate($progrStep, $progrStart);
+        $progrArray = generateProgression($progrStep, $progrStart);
         $hiddenNumber = rand(0, 9);
         $rightAnswer = (string) $progrArray[$hiddenNumber];
         $progrArray[$hiddenNumber] = '..';
