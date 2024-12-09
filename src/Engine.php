fdf3f72e06caecbb2e3111bbb6b8ddbf31078ef4
Liudmila <liudmilagubanova18@gmail.com>
2024-12-09 17:59:01 +0100

    add changes

--- src/Engine.php
+++ src/Engine.php
@@ -8,7 +8,7 @@ use function cli\prompt;
 const NUMBER_OF_ROUNDS = 3;
 function play(array $mathQuestion, string $question)
 {
-    line('Welcome to the Brain Games!');
+    line('Welcome to the Brain Games!!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
     line($question);
